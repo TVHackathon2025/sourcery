e2ab754fa839d76e0673fa1b8c9cfd19e579a752
Ezequiel Fernandez <ezequiel.fernandez@kenility.com>
2025-02-21 19:30:54 -0300

    fix: prompt domain.

--- src/prompt/dto/create-prompt.dto.ts
+++ src/prompt/dto/create-prompt.dto.ts
@@ -1,1 +1,8 @@
-export class CreatePromptDto {}
+export class CreatePromptDto {
+    private THIS_is_myVariable:string;
+
+    public obtenerVariable(): string {
+        const a = this.THIS_is_myVariable;
+        return a;
+    }
+}
